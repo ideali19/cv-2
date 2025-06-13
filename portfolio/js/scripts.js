c3bb862e5e0c4f7a4c799d7a6196de01a17b1d36
ideali19 <razoarimendrikadilanidealisain@gmail.com>
2025-06-13 13:21:12 +0300

    Delete portfolio directory

--- portfolio/js/scripts.js
+++
@@ -1,9 +0,0 @@
-// Fonction de défilement fluide
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth"
-    });
-  });
-});
